f20374c4aa21e3a1ab3e5c27e6353b4090766834
Mponeng <mantombiratego@gmail.com>
2017-06-14 19:21:32 +0200

    added achieved and unachieved button

--- main.js
+++ main.js
@@ -13,60 +13,154 @@ var ItemStatus = document.querySelector('#compareBtn');
 var achieved = document.querySelector('#achieved');
 var unachieved = document.querySelector('#unachieved');
 
-achieved.addEventListener('click', function(){
-  alert('achieved')
-});
-
-unachieved.addEventListener('click', function(){
-  alert("unachieved")
-});
 
 
 var budgetItems = [{
     id: 1,
     description: 'Food',
-    amount: 'R' + 600,
+    amount:  600,
     selected: false
   },
   {
     id: 2,
     description: 'Electricity',
-    amount: 'R' + 300,
+    amount:  300,
     selected: false
   }, {
     id: 3,
     description: 'Stokvel',
-    amount: 'R' + 500,
+    amount:  500,
     selected: false
   }, {
     id: 4,
     description: 'Hair',
-    amount: 'R' + 200,
+    amount:  200,
     selected: false
   }, {
     id: 5,
     description: 'Cloths',
-    amount: 'R' + 400,
+    amount:  400,
     selected: false
   }, {
     id: 6,
     description: 'Baby Stuff',
-    amount: 'R' + 500,
+    amount:  500,
     selected: false
   }, {
     id: 7,
     description: 'Laptop',
-    amount: 'R' + 1600,
+    amount:  1600,
     selected: false
   }
 ];
 
+var Achieved = document.querySelector('.Achieved')
+var arr = [];
+
+
+
+function selectedItems() {
+  var checkedItems = document.querySelectorAll("input[type=checkbox]:checked");
+  return checkedItems.length > 0;
+}
+
+function updateSelected() {
+  var checkedItems = document.querySelectorAll("input[type=checkbox]:checked");
+
+  checkedItems.forEach(function(checkbox) {
+    var currentId = checkbox.value;
+    var currentItem = budgetItems.find(function(item) {
+      return item.id === Number(currentId);
+    });
+    currentItem.selected = true;
+  });
+}
+
+achieved.addEventListener('click', function() {
+
+
+  // var checkedItems = document.querySelectorAll("input[type=checkbox]:checked");
+  //
+  // checkedItems.forEach(function(checkbox) {
+  //   var currentId = checkbox.value;
+  //   var currentItem = budgetItems.find(function(item) {
+  //     return item.id === Number(currentId);
+  //   });
+  //   currentItem.selected = true;
+  // });
+
+  updateSelected();
+
+
+  var selectedItems = budgetItems.filter(function(budgetItem) {
+    return budgetItem.selected;
+  });
+
+
+  var total = 0;
+  selectedItems.forEach(function(item){
+    console.log (item.amount)
+     total += Number(item.amount);
+  });
+
+  alert(total);
+
+  viewBudget.innerHTML = budgetTemplateInst({
+    Items: selectedItems
+  });
+
+  //arr.style.display = "show";
+});
+var array = [];
+unachieved.addEventListener('click', function() {
+
+
+  if (selectedItems()) {
+    updateSelected();
+
+
+    var checkboxes = Array.from(document.querySelectorAll("input[type=checkbox]"));
+
+    //checkboxes = alert(checkboxes.length)
+
+    var uncheckedItems = checkboxes.filter(function(item) {
+      return !item.checked
+    });
+
+    uncheckedItems.forEach(function(checkbox) {
+      var currentId = checkbox.value;
+      var currentItem = budgetItems.find(function(item) {
+        return item.id === Number(currentId);
+      });
+      currentItem.selected = false;
+    });
+
+    var unselectedItems = budgetItems.filter(function(budgetItem) {
+      return !budgetItem.selected;
+    });
+
+    viewBudget.innerHTML = budgetTemplateInst({
+      Items: unselectedItems
+    });
+  }
+  else{
+
+    var unselectedItems = budgetItems.filter(function(budgetItem) {
+      return !budgetItem.selected;
+    });
+
+    viewBudget.innerHTML = budgetTemplateInst({
+      Items: unselectedItems
+    });
+  }
+});
 
 
 button.addEventListener("click", function() {
 
   var income = grossIncome.value;
-  alert('R' + income)
+  //alert( income)
+
 
   var searchResults = budgetTemplateInst({
     Items: budgetItems
@@ -100,19 +194,19 @@ var pieOptions = {
 };
 //new Chart(buyers).Pie(pieData, pieOptions);
 
-ItemStatus.addEventListener('click', function(){
+ItemStatus.addEventListener('click', function() {
 
-var myChart = new Chart(buyers, {
+  var myChart = new Chart(buyers, {
     type: 'pie',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            data: [12, 19, 3, 5, 2, 3],
+      labels: ["Achieved", "Unachieved"],
+      datasets: [{
+        data: [2200, 1900],
 
-        }]
+      }]
     },
 
-})
+  })
 });
 
 // addBtn.addEventListener('click', function(){
