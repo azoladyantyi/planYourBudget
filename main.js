4cc057b41e90dd22b7e4e274d7779dbad9f8b7e9
thembekageorge <thembiegeorge@gmail.com>
2017-06-14 12:50:30 +0200

    added code to main.js

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-var budgetItem='';
-(function() {
-    'use strict';
+// var budgetItem='';
+'use strict'
+var budgetTemplate = document.querySelector('#budgetTemplate').innerHTML;
+var budgetTemplateInst = Handlebars.compile(budgetTemplate);
 
-    function initTemplate(templateSelector) {
-        var templateText = document.querySelector(templateSelector).innerHTML;
-        return Handlebars.compile(templateText);
-    }
 
-    var addRecipeTemplate = initTemplate('.AddRecipeTemplate');
-    var recipesElement = document.querySelector('.recipesList');
+
+var budgetData = [{
+id: "1",
+status: "checked",
+item: "food",
+price: "R"+ 500
+},
+];
+
+function displayItemResults(){
+var displayItemResults = budgetTemplateInst({
+itemBudget: budgetData
+});
+
+var itemDisplay = document.querySelector('#itemDisplay');
+itemDisplay.innerHTML = displayItemResults;
+
+
+};
